c629d3e86b11e24aa03ed86d55e982fcfe1ce185
Michael Christenson II <michael.christenson.ii@ibm.com>
2020-02-27 11:58:48 -0500

    Renamed package  to appropriate 'bitty'

--- bitty.go
+++ bitty.go
@@ -1,7 +1,7 @@
 // Conforms to IEC and SI Standards
 // Conversions taken from SI Brochure 9, EN, Chapter 3, page 143 (145)
 // https://www.bipm.org/utils/common/pdf/si-brochure/SI-Brochure-9.pdf
-package main
+package bitty
 
 /*
 	Copyright 2020 IBM
--- conversion_helpers.go
+++ conversion_helpers.go
@@ -1,4 +1,4 @@
-package main
+package bitty
 
 /*
 	Copyright 2020 IBM
